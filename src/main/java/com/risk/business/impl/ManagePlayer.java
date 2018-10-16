9b175b127ba7b086e7dbb7964c61082849c1872b
LAPTOP-HO056VV9\Admin <himansipatel1994@gmail.com>
2018-10-15 23:44:19 -0400

    ** R01 | SP01 | Stratup Phase Setup **
    ## Functions:
    > Player Creation  - Initializing Player's Id, Name, Initial Stock of amry assigned, Territories assiged
    > Initial Territory allocation to Players
    > Initial army distribution among Players

---
+++ src/main/java/com/risk/business/impl/ManagePlayer.java
@@ -0,0 +1,150 @@
+package com.risk.business.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+
+import com.risk.business.IManagePlayer;
+import com.risk.model.Card;
+import com.risk.model.Continent;
+import com.risk.model.GamePlayTerritory;
+import com.risk.model.Map;
+import com.risk.model.Player;
+import com.risk.model.Territory;
+
+/**
+ * This class is responsible for setting up Stratup Phase of the Game.
+ * 
+ * @author <a href="mailto:himansipatel1994@gmail.com">Himansi Patel</a>
+ * @version 0.0.1
+ */
+public class ManagePlayer implements IManagePlayer {
+
+	private List<Player> player_info_list;
+	int army_stock;
+	private List<GamePlayTerritory> total_territory_list;
+	private ManageMap manage_map_object;
+	private Map map;
+	private HashMap<String, Continent> continents;
+	Continent map_continent;
+	Territory map_territory;
+	GamePlayTerritory game_play_territory;
+
+	public ManagePlayer() {
+		player_info_list = new ArrayList<Player>();
+		manage_map_object = new ManageMap();
+		map = new Map();
+		continents = new HashMap<String, Continent>();
+		total_territory_list = new ArrayList<GamePlayTerritory>();
+	}
+
+	/**
+	 * @see com.risk.business.IManagePlayer#createPlayer(java.lang.Integer,java.lang.String)
+	 * @author <a href="mailto:himansipatel1994@gmail.com">Himansi Patel</a>
+	 */
+	@Override
+	public List<Player> createPlayer(int num_of_players, String map_name) {
+
+		army_stock = getArmyStock(num_of_players);
+		for (int i = 1; i <= num_of_players; i++) {
+			String player_name = "player" + i;
+			List<GamePlayTerritory> gameplay_territory_list = new ArrayList<>();
+			List<Card> card_list = new ArrayList<Card>();
+
+			Player p = new Player();
+
+			p.setId(i);
+			p.setName(player_name);
+			p.setArmy_stock(army_stock);
+			p.setTerritory_list(gameplay_territory_list);
+			p.setCard_list(card_list);
+
+			player_info_list.add(p);
+		}
+		map = manage_map_object.getFullMap(map_name);
+
+		assingTerritoriesToPlayers(map);
+
+		return player_info_list;
+	}
+
+	/**
+	 * This method is responsible for an initial distribution of armies in Startup
+	 * Phase of the game.
+	 * 
+	 * @author <a href="mailto:himansipatel1994@gmail.com">Himansi Patel</a>
+	 * @param num_of_players Number of players playing the game
+	 * @return Army count being received by each Player.
+	 */
+	private int getArmyStock(int num_of_players) {
+		if (num_of_players == 2) {
+			army_stock = 25;
+		} else if (num_of_players == 3) {
+			army_stock = 20;
+		} else if (num_of_players == 4) {
+			army_stock = 15;
+		} else if (num_of_players == 5) {
+			army_stock = 10;
+		} else if (num_of_players == 6) {
+			army_stock = 5;
+		}
+		return army_stock;
+	}
+
+	/**
+	 * This method is an abstraction for the process of initially allocating
+	 * territories to each player, in a round robin fashion.
+	 * 
+	 * @author <a href="mailto:himansipatel1994@gmail.com">Himansi Patel</a>
+	 * @param map Map Object for the active map in Game.
+	 */
+	private void assingTerritoriesToPlayers(Map map) {
+		List<GamePlayTerritory> territories = getTerritories(map);
+		int total_player = player_info_list.size();
+		int count = -1;
+		for (int i = 0; i < territories.size(); i++) {
+			count++;
+			if (player_info_list.get(count) != null) {
+				try {
+					player_info_list.get(count).getTerritory_list().add(territories.get(i));
+				} catch (NullPointerException e) {
+					e.printStackTrace();
+				}
+			} else {
+				continue;
+			}
+			if (count == total_player - 1) {
+				count = -1;
+			}
+		}
+	}
+
+	/**
+	 * This method is randomly linking territories to
+	 * 
+	 * @author <a href="mailto:himansipatel1994@gmail.com">Himansi Patel</a>
+	 * @param map
+	 * @return
+	 */
+	private List<GamePlayTerritory> getTerritories(Map map) {
+		continents = map.getContinents();
+		List<Territory> temp_territory_list;
+		String continent_name;
+		for (Entry<String, Continent> m : continents.entrySet()) {
+			continent_name = m.getKey();
+			map_continent = m.getValue();
+			temp_territory_list = map_continent.getTerritories();
+			for (int i = 0; i < temp_territory_list.size(); i++) {
+				map_territory = temp_territory_list.get(i);
+				game_play_territory = new GamePlayTerritory();
+				game_play_territory.setTerritory_name(map_territory.getName());
+				game_play_territory.setContinent_name(continent_name);
+				game_play_territory.setNumber_of_armies(0);
+				total_territory_list.add(game_play_territory);
+			}
+		}
+		return total_territory_list;
+	}
+
+}
