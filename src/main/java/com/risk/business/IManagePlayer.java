6ad7ba071701e79318e2c6f6084df0fec7beb3bf
Mayank Jariwala <mayankjariwala1994@gmail.com>
2018-10-19 06:16:28 -0400

    **R01 | SP01 |  Function for writeToFile remove from Interface of Manage Player **

--- src/main/java/com/risk/business/IManagePlayer.java
+++ src/main/java/com/risk/business/IManagePlayer.java
@@ -19,18 +19,4 @@ public interface IManagePlayer {
 	 * @return Entire Player List which will be rendered for Playing.
 	 */
 	List<Player> createPlayer(int num_of_players, String map_name);
-
-	/**
-	 * This method is an abstraction for the process of converting player Object
-	 * into the Game Play File to be saved/loaded.
-	 * 
-	 * @author <a href="mailto:himansipatel1994@gmail.com">Himansi Patel</a>
-	 * @param player_info_list This is the entire Player Object which will be
-	 *                         converted to a Game Play File Object and then written
-	 *                         on to a GamePlay File.
-	 * @param file_name        Name of the Map file to be stored in Resource Folder
-	 *                         and GamePlay File.
-	 * @return File write status
-	 */
-	boolean writePlayerToFile(List<Player> player_info_list, String file_name);
 }
