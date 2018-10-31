f65c2e7ae429d0236b9327b7b82fa6c5915d19ff
Himansi Patel <himansipatel1994@gmail.com>
2018-10-31 01:42:13 -0400

    ** R01 | SP02 | Army allocation in startup phase - based on army allocation type (like Automatic or Manual) ** >>Added army_allocation_type attribute to method createPlayer in interface IManagePlayer. >>Created method assignArmiesOnTerritories for assigning armies during auto allocation mode. >>Added trade_count data member in Player model to identify how many card sets are traded for a particular player.

--- src/main/java/com/risk/business/IManagePlayer.java
+++ src/main/java/com/risk/business/IManagePlayer.java
@@ -12,9 +12,11 @@ public interface IManagePlayer {
 	 * This method is an abstraction for the process of retrieving A Player Object
 	 * 
 	 * @author <a href="mailto:himansipatel1994@gmail.com">Himansi Patel</a>
-	 * @param num_of_players Number of players playing the game
-	 * @param map_name       Selected Map for playing the game.
+	 * @param num_of_players       Number of players playing the game
+	 * @param map_name             Selected Map for playing the game.
+	 * @param army_allocation_type Deciding factor for allocation of armies(M-Manual
+	 *                             or A-Automatic) on territory
 	 * @return Entire Player List which will be rendered for Playing.
 	 */
-	GamePlay createPlayer(int num_of_players, String map_name);
+	GamePlay createPlayer(int num_of_players, String map_name, String army_allocation_type);
 }
