7be5ce2c5cc560886a168696c5abc30dbef4b903
Zinnia Rana <zinnia.rana.22@gmail.com>
2018-11-05 04:02:42 -0500

    ** R01 | SP02 | Singleton Pattern Removed for attack and fortification instance and 4 Test cases added for fortification phase and 2 Test cases for Attack Phase **

--- src/main/test/com/risk/business/ManagePlayerTest.java
+++ src/main/test/com/risk/business/ManagePlayerTest.java
@@ -3,6 +3,7 @@
  */
 package com.risk.business;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
@@ -26,17 +27,19 @@ public class ManagePlayerTest {
 
 	@Before
 	public void initManagePlayer() {
+		game_play = new GamePlay();
 		manage_player = new ManagePlayer();
 	}
 
 	@Test
 	public void checkValidAttackTestDiceGreater() {
-		Attack attack = GamePlay.getAttack();
+		Attack attack = new Attack();
 		game_play = manage_player.createPlayer(2, "Switzerland.map", "A");
 		attack.setAttacker_territory("Neuchtel");
 		attack.setDefender_territory("Varduz");
 		attack.setAttacker_dice_no(6);
 		attack.setDefender_dice_no(2);
+		game_play.setAttack(attack);
 		game_play = manage_player.attack(game_play);
 		String message = game_play.getStatus();
 		assertTrue(containsInvalid(message));
@@ -44,47 +47,101 @@ public class ManagePlayerTest {
 
 	@Test
 	public void checkValidAttackTestDiceEqualLesser() {
-		Attack attack = GamePlay.getAttack();
+		Attack attack = new Attack();
 		game_play = manage_player.createPlayer(2, "Switzerland.map", "A");
 		attack.setAttacker_territory("Neuchtel");
 		attack.setDefender_territory("Varduz");
 		attack.setAttacker_dice_no(3);
 		attack.setDefender_dice_no(2);
+		game_play.setAttack(attack);
 		game_play = manage_player.attack(game_play);
 		String message = game_play.getStatus();
 		assertTrue(containsWon(message));
 	}
 
+	/**
+	 * This test just check for <i>Invalid</i> fortification move to player <b>
+	 * Neighboring Territory </b>
+	 * 
+	 * @author <a href="mailto:mayankjariwala1994@gmail.com">Mayank Jariwala</a>
+	 *         Function and Comments modification by Mayank Jariwala
+	 * @author <a href="mailto:zinnia.rana.22@gmail.com">Zinnia Rana</a>
+	 */
 	@Test
-	public void checkValidFortifyTestDestination() {
-		Fortification fortify = game_play.getFortify();
+	public void checkFortifyPhaseInvalidNeighbouringTerritoryTest() {
+		Fortification fortify = new Fortification();
 		game_play = manage_player.createPlayer(2, "Switzerland.map", "A");
 		fortify.setSource_territory("Neuchtel");
 		fortify.setDestination_territory("Varduz");
+		fortify.setArmy_count(3);
+		game_play.setFortification(fortify);
 		game_play = manage_player.fortify(game_play);
 		String message = game_play.getStatus();
-		assertTrue(containsInvalidDestination(message));
+		assertEquals("Invalid Fortification - Varduz is not a Neighbouring Territory", message);
 	}
 
+	/**
+	 * This test just check for <i>Valid</i> fortification move to player <b>
+	 * Neighboring Territory </b>
+	 * 
+	 * @author <a href="mailto:mayankjariwala1994@gmail.com">Mayank Jariwala</a>
+	 *         Function and Comments modification by Mayank Jariwala
+	 * @author <a href="mailto:zinnia.rana.22@gmail.com">Zinnia Rana</a>
+	 */
 	@Test
-	public void checkValidFortifyTestSourceArmyCount() {
-		Fortification fortify = game_play.getFortify();
+	public void checkFortifyPhaseValidNeighbouringTerritoryTest() {
+		Fortification fortify = new Fortification();
 		game_play = manage_player.createPlayer(2, "Switzerland.map", "A");
 		fortify.setSource_territory("Neuchtel");
-		fortify.setDestination_territory("Varduz");
+		fortify.setDestination_territory("Lusanne Canton");
+		fortify.setArmy_count(3);
+		game_play.setFortification(fortify);
 		game_play = manage_player.fortify(game_play);
 		String message = game_play.getStatus();
-		assertTrue(containsInvalidArmyCount(message));
+		assertEquals("Fortification Successful", message);
 	}
 
-	@Ignore
-	public boolean containsInvalidArmyCount(String s) {
-		return s.matches(".*Invalid Army Count.*");
+	/**
+	 * Test to check if there is only 1 army on source territory and player want to
+	 * move from source to destination then as per risk rule player is not allow to
+	 * 
+	 * 
+	 * @author <a href="mailto:mayankjariwala1994@gmail.com">Mayank Jariwala</a>
+	 */
+	@Test
+	public void checkFortifyInvalidFortificationArmyMoveTest() {
+		Fortification fortify = new Fortification();
+		fortify.setSource_territory("Neuchtel");
+		fortify.setDestination_territory("Bern Canton");
+		fortify.setArmy_count(3);		
+		game_play = manage_player.createPlayer(2, "Switzerland.map", "A");
+		game_play.setFortification(fortify);
+		game_play.getGame_state().get(1).getTerritory_list().get(0).setNumber_of_armies(1);
+		game_play = manage_player.fortify(game_play);
+		String message = game_play.getStatus();
+		assertEquals("Neuchtel is not having minimum armies to transfer", message);
 	}
 
-	@Ignore
-	public boolean containsInvalidDestination(String s) {
-		return s.matches(".*Invalid Destination chosen.*");
+	/**
+	 * Test to check if there is more than one army on source territory and player
+	 * want to move from source to destination then as per risk rule player is allow
+	 * to move to few armies destination territory
+	 * 
+	 * 
+	 * @author <a href="mailto:mayankjariwala1994@gmail.com">Mayank Jariwala</a>
+	 */
+	@Test
+	public void checkFortifyValidFortificationArmyMoveTest() {
+		Fortification fortify = new Fortification();
+		game_play = manage_player.createPlayer(2, "Switzerland.map", "A");
+		game_play.getGame_state().get(1).getTerritory_list().get(0).setNumber_of_armies(4);
+		fortify.setSource_territory("Neuchtel");
+		fortify.setDestination_territory("Bern Canton");
+		fortify.setArmy_count(3);
+		game_play.setFortification(fortify);
+		game_play = manage_player.fortify(game_play);
+		String message = game_play.getStatus();
+		assertEquals("Fortification Successful", message);
 	}
 
 	@Ignore
