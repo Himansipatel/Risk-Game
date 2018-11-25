62122649c4fa4eaefa95bf685a2196ff34008cc2
Himansi Patel <himansipatel1994@gmail.com>
2018-11-24 20:50:12 -0500

    ** R01 | SP03 |  Saving and Loading GamePlay at File Access Layer**

--- src/main/java/com/risk/file/IManageGamePlayFile.java
+++ src/main/java/com/risk/file/IManageGamePlayFile.java
@@ -11,25 +11,24 @@ import com.risk.model.GamePlay;
  *         Added Interface comments
  * @version 0.0.1
  */
-/**
- * @author MayankJariwala
- *
- */
 public interface IManageGamePlayFile {
 
 	/**
 	 * This function is use to write player data to new or existing Game Play file.
 	 * 
+	 * @author <a href="mailto:himansipatel1994@gmail.com">Himansi Patel</a>
 	 * @param gamePlay game_state State of Game during any phase
 	 * @return File Save Status Message
 	 */
 	Boolean saveGameStateToDisk(GamePlay gamePlay);
 
 	/**
+	 * This function is use to load the state of ongoing and saved state of game.
+	 * 
 	 * @author <a href="mayankjariwala1994@gmail.com"> Mayank Jariwala </a>
 	 * @param file_name The Playing Game File Name
-	 * @return Filename : The current ongoing game state object
+	 * @return GamePlay : The current ongoing game state object
 	 */
-	GamePlay fetchGameState(String file_name);
+	GamePlay fetchGamePlay(String file_name);
 
 }
